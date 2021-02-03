f0ec78e76f0c5ce0d3110984dc2a6f866566e2c5
Lagashettik <Lagashettik@gmail.com>
2021-02-03 21:12:55 +0530

    Regex for Email Validation

--- src/UserRegistration.java
+++ src/UserRegistration.java
@@ -3,38 +3,50 @@ import java.util.regex.*;
 
 public class UserRegistration {
     String RegexFirstLastName = "^[A-Z]{1}[a-z]{2}[a-z]*";  //For First and Last Name Regex is same
+    String RegexEmail = "^([a-zA-Z0-9]*[+._-]*[a-zA-Z0-9]+@[a-zA-Z]+.{3}[a-zA-z.]*[a-zA-z]{2})+$";
 
     Boolean CheckPattern(String Pat, String Details){
+
         Pattern pat = Pattern.compile(Pat);
         Matcher match = pat.matcher(Details);
-        boolean result = match.matches();
+        boolean result;
+        result = match.matches();
         return result;
     }
 
-    void Choice(UserRegistration User){
+    void Choice(UserRegistration User) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("1 : Firstname\n2 : Lastname");
+        System.out.println("1 : Firstname\n2 : Lastname\n3 : Email");
         System.out.println("Enter your choice");
         int choice = scan.nextInt();
         System.out.println("Enter Details");
         String Details = scan.next();
-
-        switch (choice){
-            case 1:
-                if(CheckPattern(User.RegexFirstLastName,Details) == true)
+        boolean check;
+        switch (choice) {
+            case 1 -> {
+                check = CheckPattern(User.RegexFirstLastName, Details);
+                if (check)
                     System.out.println("First Name is Valid");
                 else
                     System.out.println("First Name is Invalid");
-                break;
-            case 2:
-                if(CheckPattern(User.RegexFirstLastName,Details) == true)
+            }
+            case 2 -> {
+                check = CheckPattern(User.RegexFirstLastName, Details);
+                if (check)
                     System.out.println("Last Name is Valid");
                 else
                     System.out.println("Last Name is Invalid");
-                break;
-            default:
-                System.out.println("exit");
+            }
+            case 3 -> {
+                check = CheckPattern(User.RegexEmail, Details);
+                if (check)
+                    System.out.println("Email is Valid");
+                else
+                    System.out.println("Email is Invalid");
+            }
+            default -> System.out.println("exit");
         }
+
     }
     public static void main(String[] args) {
         UserRegistration User = new UserRegistration();
