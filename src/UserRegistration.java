0b2a58da3b87da63c6406d2290711fd343ae3183
Lagashettik <Lagashettik@gmail.com>
2021-02-03 14:21:37 +0530

    Added: Firstname validation Regex

--- src/UserRegistration.java
+++ src/UserRegistration.java
@@ -1,2 +1,25 @@
+import java.util.*;
+import java.util.regex.*;
+
 public class UserRegistration {
+    String RegexFirstName = "^[A-Z]{1}[a-z]{2}[a-z]*";
+
+
+    Boolean CheckPattern(String Pat, String Details){
+        Pattern pat = Pattern.compile(Pat);
+        Matcher match = pat.matcher(Details);
+        boolean result = match.matches();
+        return result;
+    }
+
+    public static void main(String[] args) {
+        UserRegistration User = new UserRegistration();
+        Scanner scan = new Scanner(System.in);
+        String Details = scan.next();
+        if(User.CheckPattern(User.RegexFirstName,Details) == true)
+            System.out.println("First Name is Valid");
+        else
+            System.out.println("First Name is Invalid");
+
+    }
 }
