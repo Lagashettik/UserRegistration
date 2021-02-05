68c860bad99784bbd2a8ace0670053dd961ffc42
Lagashettik <Lagashettik@gmail.com>
2021-02-05 20:15:09 +0530

    JUnit testing of User Registration added

--- Test/com/user/registration/UserRegistrationTest.java
+++ Test/com/user/registration/UserRegistrationTest.java
@@ -1,4 +1,68 @@
 package com.user.registration;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class UserRegistrationTest {
+    UserRegistrationValidation User = new UserRegistrationValidation();
+
+    @Test
+    public void ValidFirstNameTest(){
+        boolean result = User.ValidateFirstName("Krishna");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void InvalidFirstNameTest(){
+        boolean result = User.ValidateFirstName("krishna");
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void ValidLastNameTest(){
+        boolean result = User.ValidateLastName("Krishna");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void InvalidLastNameTest(){
+        boolean result = User.ValidateLastName("krishna");
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void ValidEmailTest(){
+        boolean result = User.ValidateEmail("Gameplay@gmail.com");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void InvalidEmailTest(){
+        boolean result = User.ValidateEmail("gameplaygmail.com");
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void ValidPasswordTest(){
+        boolean result = User.ValidatePassword("Gameplay@123");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void InvalidPasswordTest(){
+        boolean result = User.ValidatePassword("gameplay@123");
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void ValidMobileNumberTest(){
+        boolean result = User.ValidateMobileFormat("91 9632587410");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void InvalidMobileNumberTest(){
+        boolean result = User.ValidateMobileFormat("915232587410");
+        Assert.assertFalse(result);
+    }
 }
