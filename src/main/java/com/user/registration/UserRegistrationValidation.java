fdcd4249a59447b008cb3808c9e2b9b3a04e5cd2
Lagashettik <Lagashettik@gmail.com>
2021-03-11 17:53:18 +0530

    Modified: Validation functions to Lambda functions

--- src/main/java/com/user/registration/UserRegistrationValidation.java
+++ src/main/java/com/user/registration/UserRegistrationValidation.java
@@ -5,52 +5,20 @@ import java.util.regex.Pattern;
 public class UserRegistrationValidation {
     UserRegistration User = new UserRegistration();
 
-    public boolean ValidateFirstName(String FirstName) throws UserRegistrationException {
-        try {
-            Pattern pattern = Pattern.compile(User.RegexFirstLastName);
-            return pattern.matcher(FirstName).matches();
-        }
-        catch (Exception e){
-            throw new UserRegistrationException("invalid entry");
-        }
+    UserValidator ValidateFirstName = (s) -> (Pattern.compile(User.RegexFirstLastName).matcher(s).matches());
+
+    UserValidator ValidateLastName = (s) -> (Pattern.compile(User.RegexFirstLastName).matcher(s).matches());
+
+    UserValidator ValidateEmail = (s) -> (Pattern.compile(User.RegexEmail).matcher(s).matches());
+
+    UserValidator ValidateMobileFormat = (s) -> (Pattern.compile(User.RegexMobileFormat).matcher(s).matches());
+
+    UserValidator ValidatePassword = (s) -> (Pattern.compile(User.RegexPassword).matcher(s).matches());
+
+
+    @FunctionalInterface
+    interface UserValidator{
+        boolean validate(String s);
     }
 
-    public boolean ValidateLastName(String LastName) throws UserRegistrationException {
-        try {
-            Pattern pattern = Pattern.compile(User.RegexFirstLastName);
-            return pattern.matcher(LastName).matches();
-        }
-        catch (Exception e){
-            throw new UserRegistrationException("invalid entry");
-        }
-    }
-
-    public boolean ValidateEmail(String Email) throws UserRegistrationException {
-        try {
-            Pattern pattern = Pattern.compile(User.RegexEmail);
-            return pattern.matcher(Email).matches();
-        }
-        catch (Exception e){
-            throw new UserRegistrationException("invalid entry");
-        }
-    }
-
-    public boolean ValidateMobileFormat(String MobileNumber) throws UserRegistrationException {
-        try {
-            Pattern pattern = Pattern.compile(User.RegexMobileFormat);
-            return pattern.matcher(MobileNumber).matches();
-        }
-        catch (Exception e){
-            throw new UserRegistrationException("invalid entry");
-        }
-    }
-
-    public boolean ValidatePassword(String Password) throws UserRegistrationException {
-        try{Pattern pattern = Pattern.compile(User.RegexPassword);
-        return pattern.matcher(Password).matches();
-    }
-        catch (Exception e){
-            throw new UserRegistrationException("invalid entry");
-        }
-    }
 }
--- src/test/java/com/user/registration/UserRegistrationTest.java
+++ src/test/java/com/user/registration/UserRegistrationTest.java
@@ -7,62 +7,62 @@ public class UserRegistrationTest {
     UserRegistrationValidation User = new UserRegistrationValidation();
 
     @Test
-    public void ValidFirstNameTest() throws UserRegistrationException {
-        boolean result = User.ValidateFirstName("Krishna");
+    public void ValidFirstNameTest(){
+        boolean result = User.ValidateFirstName.validate("Krishna");
         Assert.assertTrue(result);
     }
 
     @Test
-    public void InvalidFirstNameTest() throws UserRegistrationException{
-        boolean result = User.ValidateFirstName("krishna");
+    public void InvalidFirstNameTest(){
+        boolean result = User.ValidateFirstName.validate("krishna");
         Assert.assertFalse(result);
     }
 
     @Test
-    public void ValidLastNameTest() throws UserRegistrationException{
-        boolean result = User.ValidateLastName("Krishna");
+    public void ValidLastNameTest(){
+        boolean result = User.ValidateLastName.validate("Krishna");
         Assert.assertTrue(result);
     }
 
     @Test
-    public void InvalidLastNameTest() throws UserRegistrationException{
-        boolean result = User.ValidateLastName("krishna");
+    public void InvalidLastNameTest(){
+        boolean result = User.ValidateLastName.validate("krishna");
         Assert.assertFalse(result);
     }
 
     @Test
-    public void ValidEmailTest() throws UserRegistrationException{
-        boolean result = User.ValidateEmail("Gameplay@gmail.com");
+    public void ValidEmailTest(){
+        boolean result = User.ValidateEmail.validate("Gameplay@gmail.com");
         Assert.assertTrue(result);
     }
 
     @Test
     public void InvalidEmailTest() throws UserRegistrationException{
-        boolean result = User.ValidateEmail("gameplaygmail.com");
+        boolean result = User.ValidateEmail.validate("gameplaygmail.com");
         Assert.assertFalse(result);
     }
 
     @Test
-    public void ValidPasswordTest() throws UserRegistrationException{
-        boolean result = User.ValidatePassword("Gameplay@123");
+    public void ValidPasswordTest(){
+        boolean result = User.ValidatePassword.validate("Gameplay@123");
         Assert.assertTrue(result);
     }
 
     @Test
-    public void InvalidPasswordTest() throws UserRegistrationException{
-        boolean result = User.ValidatePassword("gameplay@123");
+    public void InvalidPasswordTest(){
+        boolean result = User.ValidatePassword.validate("gameplay@123");
         Assert.assertFalse(result);
     }
 
     @Test
-    public void ValidMobileNumberTest() throws UserRegistrationException{
-        boolean result = User.ValidateMobileFormat("91 9632587410");
+    public void ValidMobileNumberTest(){
+        boolean result = User.ValidateMobileFormat.validate("91 9632587410");
         Assert.assertTrue(result);
     }
 
     @Test
-    public void InvalidMobileNumberTest() throws UserRegistrationException{
-        boolean result = User.ValidateMobileFormat("915232587410");
+    public void InvalidMobileNumberTest(){
+        boolean result = User.ValidateMobileFormat.validate("915232587410");
         Assert.assertFalse(result);
     }
 }
